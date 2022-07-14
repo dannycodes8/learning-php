3d033525407f1714ca5f4bb69e478811b5ceb625
DannyCodes <54466953+dannycodes8@users.noreply.github.com>
2022-07-14 15:01:25 +0300

    Add files via upload

---
+++ danny.blade.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+<body>
+
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+<h4>Login</h4>
+Name:<input type="text" name="name"><br><br>
+Email:<input type="text" name="email"><br><br>
+<input type="submit">
+
+</form>
+
+
+
+</body>
+
+</head>
+</html>
